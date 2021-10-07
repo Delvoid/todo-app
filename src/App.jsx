f53abe9d319c6713e9c7415d971c81d07b07fa20
Delvoid <delvoidltd@gmail.com>
2021-10-07 22:13:58 +0100

    removed old data from testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,9 @@ import { useState, useEffect } from 'react'
 import Tasks from './components/Tasks'
 
 import './App.css'
-const defaultData = [
-  { title: 'group 1', items: ['1', '2', '3'] },
-  { title: 'group 2', items: ['4', '5'] },
-]
 
 function App() {
   const [tasks, setTasks] = useState([])
-  const [data, setData] = useState()
-  useEffect(() => {
-    if (localStorage.getItem('List')) {
-      console.log(localStorage.getItem('List'))
-      setData(JSON.parse(localStorage.getItem('List')))
-    } else {
-      setData(defaultData)
-    }
-  }, [setData])
 
   // FORM
   const [submited, setSubmited] = useState(false)
